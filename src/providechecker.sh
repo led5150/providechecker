c6932f2fe70a486b018ed59885eedbb79604e921
kedestin <26238343+kedestin@users.noreply.github.com>
2020-08-25 00:47:52 -0400

    Refactored ssh out of 'checker' modules

--- src/providechecker.sh
+++ src/providechecker.sh
@@ -1,125 +1,154 @@
 #!/bin/bash
 
-# Usage: ./provide_checker.sh homework_name file1 file2 file3 ...
-
 # Validates user has properly run program
-function validate_usage() 
-{
-    # Check to make sure user has run the program correctly
-    if [[ "$#" -lt 2 ]]; then
-        echo "Invalid number of arguments provided"
-        echo "Usage: $0 homework_name file1 file2 file3 ..."
-        exit
-    fi
+function validate_usage() {
+        # Check to make sure user has run the program correctly
+        if [[ "$#" -lt 2 ]]; then
+                echo "Invalid number of arguments provided"
+                echo "Usage: $0 homework_name file1 file2 file3 ..."
+                exit
+        fi
 }
 
 # removes line continuations from a given file
-function remove_line_continuations() 
-{
-    parsed=$(sed -e ':x /[[:space:]]*\\$/ { N; s/[[:space:]]*\\\n//g; s/[[:space:]][[:space:]]*/ /g ; bx }' "$1")
-    echo "$parsed"
+function remove_line_continuations() {
+        parsed=$(sed -e ':x /[[:space:]]*\\$/ { N; s/[[:space:]]*\\\n//g; s/[[:space:]][[:space:]]*/ /g ; bx }' "$1")
+        echo "$parsed"
 }
 
-# Sets variables we need for various purposes. Takes all arguments from 
+# Sets variables we need for various purposes. Takes all arguments from
 # command line
-function set_variables() 
-{
-    # Store homework name, user given files and set up file paths
-    REMOTE="mkorma01@homework.cs.tufts.edu"
-    HWNAME="$1"
-    shift
-    USR_FILES=("$@")
-    TEST_SET_PATH=/comp/15/grading/screening/testsets/"$HWNAME"
-    ASSN_CONF=/comp/15/grading/assignments.conf
-    PC_TEST_DIR=${BASH_SOURCE%/*}/checkers
-    TEST_DIR="$HWNAME"_pc_submission
-    mkdir "$TEST_DIR"
+function set_variables() {
+        # Store homework name, user given files and set up file paths
+        REMOTE="mkorma01@homework.cs.tufts.edu"
+        HWNAME="$1"
+        shift
+        USR_FILES=("$@")
+        TEST_SET_PATH=/comp/15/grading/screening/testsets/"$HWNAME"
+        ASSN_CONF=/comp/15/grading/assignments.conf
+        CHECKERS=${BASH_SOURCE%/*}/checkers
+        BASE_DIR="providecheck/$HWNAME"
+        mkdir -p "$BASE_DIR"
 
-    # Remove line continuations and store testset
-    TEST_SET=$(remove_line_continuations "$TEST_SET_PATH")
+        # Remove line continuations and store testset
+        TEST_SET=$(remove_line_continuations "$TEST_SET_PATH")
 }
 
-# Requires filepath of directory 
+# Requires filepath of directory
 # Directory will be created and have user files copied into
-function copy_files() 
-{
-    mkdir "$1"
-    for file in "${USR_FILES[@]}"; do
-        if ! cp "$file" "$1"; then
-            echo "Files don't exist in this directory or are spelled wrong!"
-            rm -r "$1"
-            exit 1
-        fi
-    done
+function copy_files() {
+        mkdir -p "$1"
+        cp "${USR_FILES[@]}" "$1"
+
+        # for file in "${USR_FILES[@]}"; do
+        #         if ! cp "$file" "$1"; then
+        #                 echo "Files don't exist in this directory or are spelled wrong!"
+        #                 rm -r "$1"
+        #                 exit 1
+        #         fi
+        # done
 }
 
 # Takes all arguments passed in from command line
 function setup() {
-    set_variables "$@"
-    copy_files "$TEST_DIR/passing"
-    # copy files to student level account
-    scp -q -r "$TEST_DIR" "$REMOTE":~/ProvideCheck
+        set_variables "$@"
 }
 
-# Asserts that the assignment has been added to assignments.conf 
+# Asserts that the assignment has been added to assignments.conf
 # and is uncommented
-function validate_assignment() 
-{ 
-    if ! ASSIGN=$(remove_line_continuations "$ASSN_CONF" | grep '^[^#][a-z]*='"$HWNAME"'\s'); then
-        echo "Assignment doesn't exist in $ASSN_CONF or is commented out."
-        echo "Please add the assignment to assignments.conf and try again."
-        exit 1
-    fi
+function validate_assignment() {
+        if ! ASSIGN=$(remove_line_continuations "$ASSN_CONF" | grep '^[^#][a-z]*='"$HWNAME"'\s'); then
+                echo "Assignment doesn't exist in $ASSN_CONF or is commented out."
+                echo "Please add the assignment to assignments.conf and try again."
+                exit 1
+        fi
 }
-
-# Checks pc_test directory for the test name passed in as an argument
-function check_for_test() 
-{
-    if [ -f "$PC_TEST_DIR/$1" ]; then
-        return 0
-    else
-        return 1
-    fi
-}   
 
 # Takes exit status of a given test and determines success or failure
-function assert_test()
-{
-    # echo "exit status: $1"
-    if [[ "$1" == 1 ]]; then
-        echo "~~~~~~~ $2 test failed. ~~~~~~~"
-        echo ""
-    else
-        echo "~~~~~~~ $2 test passed. ~~~~~~~"
-        echo ""
-    fi
-}
-
-# Runs tests.  Passing test always runs.  Other tests are parsed from the 
-# testset and run if they are found.
-function run_tests() 
-{
-    # we always run passing test
-    ssh "$REMOTE" 'bash -s' < $PC_TEST_DIR/passing "$HWNAME" "$TEST_DIR/passing" 
-    assert_test "$?" passing
-
-    # Next we parse the TEST_SET for tests, and execute them if they exist
-    echo "$TEST_SET" | while read line ; do   
-        if TEST_LINE=($(egrep '^(FAIL|WARN)' <<< "$line")); then
-            POSSIBLE_TEST="${TEST_LINE[1]}"
-            if check_for_test "$POSSIBLE_TEST"; then # if test exists
-                copy_files "$TEST_DIR/$POSSIBLE_TEST"
-                # run test
-                $PC_TEST_DIR/$POSSIBLE_TEST "$HWNAME" "$TEST_DIR" \
-                "$TEST_DIR/$POSSIBLE_TEST" "$REMOTE" ${TEST_LINE[@]}
-                assert_test "$?" "$POSSIBLE_TEST"
-            fi
+function assert_test() {
+        # echo "exit status: $1"
+        if [[ "$1" == 1 ]]; then
+                echo "~~~~~~~ $2 test failed. ~~~~~~~"
+                echo ""
+        else
+                echo "~~~~~~~ $2 test passed. ~~~~~~~"
+                echo ""
         fi
-    done
 }
 
+function perform_edits() {
+        i=1
+        mapfile -t LINES <<<"FAIL passing"$'\n'"$TEST_SET"
 
+        # Prepare edits by each module
+        for line in "${LINES[@]}"; do
+                if ! TEST_LINE=($(egrep '^(FAIL|WARN)' <<<"$line")); then
+                        continue
+                fi
+                TESTSET_CMD="${TEST_LINE[1]}"
+                STAGING_DIR="$BASE_DIR/${i}_$TESTSET_CMD"
+                if ! [ -f "$CHECKERS/$TESTSET_CMD" ]; then
+                        continue
+                fi
+                CHECKER=$(realpath "$CHECKERS/$TESTSET_CMD")
+                copy_files "$STAGING_DIR"
+                # run test
+                (cd "$STAGING_DIR" && $CHECKER "${TEST_LINE[@]:2}")
 
+                ((i = i + 1))
+        done
+}
+
+function remote_provide() {
+        # copy files to student level account
+        # might use rsync instead
+        ssh "$REMOTE" "rm -rf ~/$BASE_DIR && mkdir -p ~/$BASE_DIR "
+        scp -q -r "$BASE_DIR" "$REMOTE:~/$BASE_DIR/.."
+
+        # For each directory
+        # cd into it, find all files in directory and provide
+        # TODO : Shorten this
+        ssh -T "$REMOTE" bash -s <<EOF
+                function provideAll() {
+                        local files=\$(find . -maxdepth 1 -type f \
+                                      | xargs -r  basename -a)
+                        
+                        if ! [ -z "\$files" ]; then 
+                                yes | provide comp15 $HWNAME \$files > provide_output.txt
+                        fi
+
+                        for dir in */; do
+                                if ! [ -d "\$dir" ]; then
+                                        continue
+                                fi
+                                (cd \$dir && provideAll)
+                        done
+                }
+
+                cd ~/$BASE_DIR && provideAll
+
+EOF
+        # Copy provide_output.txt's back to local
+        scp -q -r "$REMOTE:~/$BASE_DIR" "$BASE_DIR"/..
+}
+
+function evaluate_results() {
+        for directory in "$BASE_DIR"/*/; do
+                dirname=$(basename "$directory")
+                cmdName=${dirname#*_}
+                cmd=$(realpath "$CHECKERS/$cmdName")
+                (cd "$directory" && $cmd --test)
+                assert_test "$?" "$cmdName"
+        done
+}
+
+# Runs tests.  Passing test always runs.  Other tests are parsed from the
+# testset and run if they are found.
+function run_tests() {
+        perform_edits
+        remote_provide
+        evaluate_results
+}
 
 # Lets run it!
 
@@ -128,9 +157,4 @@ setup "$@"
 validate_assignment
 run_tests
 
-rm -rf $TEST_DIR
-
-
-
-
-
+# rm -rf "$BASE_DIR"
